333835c932c739b61bc8336c4945ca98800607dd
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-09-02 16:24:46 +0300

    methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < Sinatra::Base
 
   # add routes
 
+  set :default_content_type, 'application/json'
+    get '/bakeries' do 
+
+      bakeries = Bakery.all
+      bakeries.to_json
+    end
+    get '/bakeries/:id' do 
+      baker = Bakery.find(params[:id])
+      baker.to_json(include: :baked_goods)
+
+    end
+    get 'baked_goods/by_price' do
+      baked_goods = BakedGood.by_price
+      baked_goods.to_json
+    end
+    get '/baked_goods/most_expensive' do
+      pricey = BakedGood.by_price.first
+            pricey.to_json
+    end
+
 end
